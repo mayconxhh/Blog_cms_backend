1264af4234f20816bffbfcc545ac3f370110835f
mayconxhh <mayconxhh@gmail.com>
2020-02-15 13:16:07 -0500

    se agrego backend de blog, usuariios, post, categorias, login, authenticacion

---
+++ app.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+var passport = require('passport');
+
+var index = require('./routes/index');
+// var usersRouter = require('./routes/users');
+
+var auth = require('./routes/auth');
+var category = require('./routes/category');
+var post = require('./routes/post');
+
+mongoose.connect('mongodb://localhost/blog-cms', {
+    promiseLibrary: require('bluebird'),
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(() =>  console.log('connection successful'))
+  .catch((err) => console.error(err));
+
+var app = express();
+
+app.use(passport.initialize());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
+app.use('/api/public', index);
+// app.use('/users', usersRouter);
+app.use('/api/auth', auth);
+app.use('/api/category', category);
+app.use('/api/post', post);
+
+module.exports = app;
